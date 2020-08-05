d47d2212f052077d79d86cc1c45feedcee0be789
Avinash <sharma.avinash96@gmail.com>
2020-08-05 19:20:58 +0530

    if tie then game continue till difference of two

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+
 playerOne=0
 playerTwo=0
 maxWin=21
@@ -16,12 +18,31 @@ else
 fi
 done
 
-if [[ $playerOne -eq $playerTwo ]]
+if [[ $(($playerOne-$playerTwo)) -eq 0 ]]
 then
-	echo "It's a Tie"
+p1=0
+p2=0
+diff=0
+while (( $diff <= 2 ))
+do
+coinFlip=$((RANDOM%2))
+if [[ $coinFlip -eq 1 ]]
+then
+        ((p1++))
+else
+        ((p2++))
+fi
+diff=$(($p1-$p2))
+done
+        if [[ $p1 -gt $p2 ]]
+        then
+                echo "Player One Won"
+        else
+                echo "PLayer two Won"
+        fi
 elif [[ $playerOne -gt $playerTwo ]]
 then
-   echo "Player One Won by difference of: " $(( $maxWin - $playerTwo ))
+        echo "Player One Won by difference of: " $(( $maxWin - $playerTwo ))
 else
 	echo "Player Two Won by difference of: " $(( $maxWin - $playerOne))
 fi
