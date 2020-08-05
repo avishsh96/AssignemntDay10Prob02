67416550685cc9e0b4e3294a9ea2afa109b2c14e
Avinash <sharma.avinash96@gmail.com>
2020-08-05 19:17:26 +0530

    to Show Win or tie and who won

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,13 +1,27 @@
 #!/bin/bash -x
-count=0
-while (( $count < 100 ))
+playerOne=0
+playerTwo=0
+maxWin=21
+
+while (( $playerOne < $maxWin && $playerTwo < $maxWin ))
 do
-((count++))
 coinFlip=$((RANDOM%2))
 if [[ $coinFlip -eq 1 ]]
 then
-        echo "Head"
+        echo "Player One won this round"
+        ((playerOne++))
 else
-        echo "Tail"
+        echo "Player Two Won this round"
+        ((playerTwo++))
 fi
 done
+
+if [[ $playerOne -eq $playerTwo ]]
+then
+	echo "It's a Tie"
+elif [[ $playerOne -gt $playerTwo ]]
+then
+   echo "Player One Won by difference of: " $(( $maxWin - $playerTwo ))
+else
+	echo "Player Two Won by difference of: " $(( $maxWin - $playerOne))
+fi
